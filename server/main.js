2ec880c1f1250d8e73b724b295028faaf22cfaef
Fang Hong <fanghong@Fangs-MacBook-Pro.local>
2019-03-17 14:41:03 -0700

    google sign in

--- server/main.js
+++ server/main.js
@@ -10,4 +10,14 @@ Meteor.startup(() => {
 		appId: '718545585214421',
 		secret: 'd91607cb955f3fbfd01277fe396d0ddf'
 	});
+
+	ServiceConfiguration.configurations.remove({
+			service: "google"
+		});
+	ServiceConfiguration.configurations.insert({
+			service: "google",
+			clientId: "219148497315-egqa4lm3jd5lb0bb62587cg72h5fngiu.apps.googleusercontent.com",
+			secret: "S0OjVAwaCpcPvgtJ3iBEDt0B"
+		});
+
 });
