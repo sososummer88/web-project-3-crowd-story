9621132a7f27859e1b45750251c46e65b933c358
beefman92 <bob_zhao@163.com>
2019-03-21 15:28:20 -0700

    remove google and facebook sign in

--- server/main.js
+++ server/main.js
@@ -7,22 +7,4 @@ import { ServiceConfiguration } from "meteor/service-configuration";
 
 
 Meteor.startup(() => {
-	ServiceConfiguration.configurations.remove({
-		service: "facebook"
-	});
-	ServiceConfiguration.configurations.insert({
-		service: "facebook",
-		appId: "718545585214421",
-		secret: "d91607cb955f3fbfd01277fe396d0ddf"
-	});
-
-	ServiceConfiguration.configurations.remove({
-		service: "google"
-	});
-	ServiceConfiguration.configurations.insert({
-		service: "google",
-		clientId: "219148497315-egqa4lm3jd5lb0bb62587cg72h5fngiu.apps.googleusercontent.com",
-		secret: "S0OjVAwaCpcPvgtJ3iBEDt0B"
-	});
-
 });
