c37d7df540eb0d0310ad1c63c239ac5a731a1a2a
beefman92 <bob_zhao@163.com>
2019-03-22 04:10:58 -0700

    fix syntax error

--- server/main.js
+++ server/main.js
@@ -3,8 +3,6 @@ import "../imports/api/story-content";
 import "../imports/api/chat-info";
 import "../imports/api/story-meta";
 import "../imports/api/corpus";
-import { ServiceConfiguration } from "meteor/service-configuration";
-
 
 Meteor.startup(() => {
 });
